c437d4d2625bfe74431305ca7caae34a932c0134
Yosuke Ishikawa <y@ishkawa.org>
2016-03-12 15:08:38 +0900

    Initial commit

--- Pagination/SearchRepositoriesViewController.swift
+++ Pagination/SearchRepositoriesViewController.swift
@@ -1,19 +1,3 @@
-# RxPagination
-
-This is the demo project for my presentation at try! Swift conference 2016. 
-
-- Slides: https://speakerdeck.com/ishkawa/protocol-oriented-programming-in-networking
-- Video: To be uploaded
-
-## Set Up
-
-- `carthage bootstrap --platform iOS`
-
-## Summary
-
-When you give a type parameter `Request` to `PaginationViewModel<Request: PaginationRequestType>`, you can get typed response stream `Observable<[Request.Response.Element]>`.
-
-```swift
 import UIKit
 import RxSwift
 
@@ -21,8 +5,6 @@ class SearchRepositoriesViewController: UITableViewController {
     @IBOutlet weak var indicatorView: UIActivityIndicatorView!
 
     let disposeBag = DisposeBag()
-
-    // PaginationViewModel<GitHubAPI.SearchRepositoriesRequest>
     let viewModel = PaginationViewModel(
         baseRequest: GitHubAPI.SearchRepositoriesRequest(query: "Swift"))
 
@@ -38,24 +20,15 @@ class SearchRepositoriesViewController: UITableViewController {
             .bindTo(viewModel.loadNextPageTrigger)
             .addDisposableTo(disposeBag)
 
-        // viewModel.loading: Variable<Bool>
         viewModel.loading.asDriver()
             .drive(indicatorView.rx_animating)
             .addDisposableTo(disposeBag)
 
-        // viewModel.elements: Variable<[Request.Response.Element]>
         viewModel.elements.asDriver()
             .drive(tableView.rx_itemsWithCellIdentifier("Cell")) { _, repository, cell in
-                // repository: Repository (= Request.Response.Element)
                 cell.textLabel?.text = repository.fullName
                 cell.detailTextLabel?.text = "🌟\(repository.stargazersCount)"
             }
             .addDisposableTo(disposeBag)
     }
 }
-```
-
-## Contact
-
-Twitter: https://twitter.com/_ishkawa
-
